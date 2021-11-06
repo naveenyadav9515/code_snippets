56136ea4de478aafd84f21226c5cccfe174b594e
naveenyadav9515 <naveenyadav9515@gmail.com>
2021-11-06 11:48:15 +0530

    added notes for reference

---
+++ angular/apicalls/Readme-notes.txt
@@ -0,0 +1,5 @@
+Reference for form upload and download - 'https://www.convertapi.com/doc/upload'
+
+It is an online service for fake file api calls. we can upload any file,
+but uploaded file will be availabel only for 3 hours.
+ 
--- angular/apicalls/src/app/services/api.service.ts
+++ angular/apicalls/src/app/services/api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { records } from '../api-calls/table-model'
 
 export interface records {
   id: number;
@@ -12,22 +11,22 @@ export interface records {
   providedIn: 'root'
 })
 export class ApiService {
-  public url = '';
+  public url = "http://localhost:3000/users";
   constructor(private http: HttpClient) { }
 
   getData(){
-    return this.http.get<records[]>("http://localhost:3000/users");
+    return this.http.get<records[]>(this.url);
   }
   putData(data:any){
-    return this.http.post("http://localhost:3000/users",data);
+    return this.http.post(this.url,data);
   }
 
   deleteRecord(id:any){
-    return this.http.delete("http://localhost:3000/users/"+id)
+    return this.http.delete(this.url+id)
   }
 
   updateRecord(data:any){
-    return this.http.put("http://localhost:3000/users/"+data.id,data);
+    return this.http.put(this.url+data.id,data);
   }
 
   getFile(url:any) {
@@ -35,7 +34,7 @@ export class ApiService {
   }
   
   upload(file: any): Observable<any> {
-    const endpoint = 'https://v2.convertapi.com/upload ';
+    const endpoint = 'https://v2.convertapi.com/upload';
     const formData: FormData = new FormData();
     formData.append('file', file, file.name);
     return this.http.post(endpoint, formData);
