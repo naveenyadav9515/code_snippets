ef8df4b772e1572701cb2c2e88e0ba4426e5e3c1
naveenyadav9515 <naveenyadav9515@gmail.com>
2021-12-09 16:16:07 +0530

    Initial commit for form snippets

---
+++ angular/forms/src/app/incident-form/incident-form.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IncidentFormRoutingModule } from './incident-form-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IncidentFormRoutingModule
+  ]
+})
+export class IncidentFormModule { }
